8d689af84d128f0f06221d6906e1cec9365af0eb
eiz <sashkaseb@mail.ru>
2024-02-04 17:01:43 +0300

    wrote the navigation

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users,
-             path_names: { sign_in: :login, sign_out: :logout },
-             controllers: { sessions: 'users/sessions'}
+             path_names: { sign_in: :login, sign_out: :logout }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
